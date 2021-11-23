6abd423d67143bc0d31b72dc8fc7ee429ccd308a
Rafael Dalli Soares <rafael.dalli@hotmail.com>
2021-11-23 18:04:13 -0300

    Upload

---
+++ SOLID/src/br/com/solid/rh/service/reajuste/ValidacaoPercentualReajuste.java
@@ -0,0 +1,21 @@
+package br.com.solid.rh.service.reajuste;
+
+import br.com.solid.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.solid.rh.ValidacaoException;
+
+public class ValidacaoPercentualReajuste implements ValidacaoReajuste {
+	
+	public void validar(Funcionario funcionario, BigDecimal aumento) {
+		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+		
+		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+	}
+
+}
